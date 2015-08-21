2080567d8dcd4cdeb4e5b681431e560811287dcd
phillip <philliparente@gmail.com>
2015-08-21 11:40:28 -0300

    changed settings to parent directory apps

--- rede_gsti/settings.py
+++ rede_gsti/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
-    'article',
-    'comment',
-    'uestion',
-    'userprofile'
+    'apps.account',
+    'apps.article',
+    'apps.comment',
+    'apps.question',
+    'apps.userprofile'
 )
 
 MIDDLEWARE_CLASSES = (
