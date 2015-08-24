6dae667845d55b3ee332595f958e02bde7437f54
phillip <philliparente@gmail.com>
2015-08-24 18:57:31 -0300

    removed prints, add login link to successfully registration view

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.http import Http404
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .service.forms import SignUpForm, LoginForm
