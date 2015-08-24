943b52975547a555d11d44fed20b561cdc0af69f
Raphael Fruneaux <raphaelfruneaux@Raphael.local>
2015-08-24 16:36:21 -0300

    Model MailValidation in account/models.py

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 # Create your models here.
+
+
+class MailValidation(models.Model):
+
+    user = models.OneToOneField(User)
+    token = models.TextField(unique=True)
+    link_date = models.DateTimeField(default=timezone.now() + timedelta(days=2))
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.token
+
+    def is_valid(self):
+        return self.link_date <= timezone.now() + timedelta(days=2)
