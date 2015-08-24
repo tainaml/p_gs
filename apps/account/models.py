8d2eb4920350b1e9136e03cc214d6b83b6915a90
Raphael Fruneaux <raphaelfruneaux@Raphael.local>
2015-08-24 17:53:13 -0300

    ajust Model MailValidation in account/models.py

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta
-from django.utils import timezone
 
 # Create your models here.
+from django.utils import timezone
 
 
 class MailValidation(models.Model):
 
     user = models.OneToOneField(User)
     token = models.TextField(unique=True)
-    link_date = models.DateTimeField(default=timezone.now() + timedelta(days=2))
+    link_date = models.DateTimeField(default=datetime.today())
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.token
 
     def is_valid(self):
-        return self.link_date <= timezone.now() + timedelta(days=2)
+        """
+        date_created = 2015-08-24 (+2days)
+        date_expire  = 2015-08-26
+        ---------------------------
+
+        fake_today   = 2015-08-24
+
+        condition = date_create + 2 days >= fake_today
+
+        :return:
+        """
+        return self.link_date + timedelta(days=2) >= timezone.now()
--- apps/account/tests.py
+++ apps/account/tests.py
@@ -1,3 +1,23 @@
+import hashlib
+import random
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import MailValidation
+
+
+class MailValidationTests(TestCase):
+
+    def test_is_valid(self):
+
+        user = User.objects.get(id=2)
+
+        salt = hashlib.sha1(str(random.random())).hexdigest()[:5]
+        activation_key = hashlib.sha1(salt+user.email).hexdigest()
+
+        print activation_key
+
+        token = MailValidation(token=activation_key, user=user)
+        self.assertEqual(token.is_valid(), False)
+
