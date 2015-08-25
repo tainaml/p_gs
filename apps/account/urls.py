ac8d56c93a8f2ced7a1e54da8a4f47fab1ab8d02
Antonio Ladeia <antonioladeia@ideiaseo.com>
2015-08-25 17:36:06 -0300

    remove signup_withou_captcha from urls

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     url(r'^logout/', views.logout, name='logout'),
     url(r'^change_password/', views.change_password, name='change_password'),
     url(r'^update_password/', views.update_password, name='update_password'),
-    url(r'^signup_without_captcha/$', views.signup_without_captcha, name='signup_without_captcha'),
     url(r'^registered-successfully/$', views.registered_successfully, name='registered_successfully'),
     url(r'^mail_validation/(?P<activation_key>[0-9a-z]{40})/$', views.mail_validation, name='mail_validation'),
     url(r'^recovery_validation/(?P<activation_key>[0-9a-z]{40})/$', views.recovery_validation, name='recovery_validation'),
