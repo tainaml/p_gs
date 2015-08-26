e7f760abe4987374b55fb23a017cf0790e209947
Anniel Menezes <annielmenezes@ideiaseo.com>
2015-08-26 17:14:05 -0300

    Add urls to ninico app and statics and templates for the same

--- apps/ninico/views.py
+++ apps/ninico/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'home/index.html')
+
+def perfil(request):
+    return render(request, 'home/perfil.html')
