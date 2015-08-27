430823a3ddb573079fc57159b6dcbf06f43c558e
phillip <philliparente@gmail.com>
2015-08-27 10:56:27 -0300

    useless import removed

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .service.forms import SignUpForm, LoginForm, ChangePasswordForm, RecoveryPasswordForm, ForgotPasswordForm
