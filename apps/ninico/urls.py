0e87253e2d428772c39d3948aefe2885b1b28d6b
Anniel Menezes <annielmenezes@ideiaseo.com>
2015-08-31 09:11:30 -0300

    Add perfil urls

--- apps/ninico/urls.py
+++ apps/ninico/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^perfil$', views.perfil, name='perfil')
+    url(r'^perfil$', views.perfil, name='perfil'),
+    url(r'^perfil/feed$', views.feed, name='feed'),
+    url(r'^perfil/about$', views.about, name='about'),
+    url(r'^perfil/relationship$', views.relationship, name='relationship'),
+    url(r'^perfil/communities$', views.communities, name='communities'),
+    url(r'^perfil/videos$', views.videos, name='videos'),
 ]
--- apps/ninico/views.py
+++ apps/ninico/views.py
@@ -5,4 +5,19 @@ def index(request):
     return render(request, 'home/index.html')
 
 def perfil(request):
-    return render(request, 'home/perfil.html', {"isLogged": False})
+    return render(request, 'home/perfil.html')
+
+def feed(request):
+    return render(request, 'home/perfil-feed.html')
+
+def about(request):
+    return render(request, 'home/perfil-sobre.html')
+
+def relationship(request):
+    return render(request, 'home/perfil-relacionamentos.html')
+
+def videos(request):
+    return render(request, 'home/perfil-videos.html')
+
+def communities(request):
+    return render(request, 'home/perfil-comunidades.html')
