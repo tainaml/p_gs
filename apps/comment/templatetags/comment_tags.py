4e37fc60afbee8f6463a0f527d6768b35ed3e4b5
phillip <philliparente@gmail.com>
2015-09-02 15:10:48 -0300

    fixed bug in content language

--- apps/comment/templatetags/comment_tags.py
+++ apps/comment/templatetags/comment_tags.py
@@ -10,15 +10,17 @@ register = template.Library()
 @register.inclusion_tag('comment/create.html', takes_context=True)
 def box_comment(context, content_object, url_next):
 
+
     try:
         content_type = ContentType.objects.get_for_model(content_object)
+
 
     except:
         raise Http404()
 
     return {
         'content_object': content_object,
-        'content_type': content_type,
+        'content_type': content_type.model,
         'url_next': url_next,
         'request': context['request']
     }
@@ -37,6 +39,7 @@ def list_comment(context, content_object):
 
     try:
         content = ContentType.objects.get_for_model(content_object)
+
         list_comment = Comment.objects.filter(content_type=content, object_id=content_object.id).order_by('-creation_date')
         max_levels = settings.MAX_LEVELS if hasattr(settings,
                                                 'MAX_LEVELS') else False
