0a7bc972f0cad951a1ab3f705e830c4ef2e7fbed
Ladeia <antonioladeia@ideiaseo.com>
2015-09-04 18:53:13 +0000

    keep data

--- apps/question/views.py
+++ apps/question/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from apps.question.models import Question
 
-# Create your views here.
+
+def list_questions(request):
+    questions = Question.objects.all()
+    return render(request, 'question/list.html', {'questions': questions})
+
+
+#TODO 'see how filter model'
+def list_question(request):
+    question = Question.objects.
+    return render(request, 'question/index_teste.html', {'question': question})
+
+@login_required
+def create_question(request):
+
+
+@login_required
+def save_question(request):
+
+
+@login_required
+def edit_question(request):
+
+
+@login_required
+def update_question(request):
+
+
+def show_reply(request):
+
+
+@login_required
+def create_reply(request):
+
+
+@login_required
+def save_reply(request):
+
+
+@login_required
+def edit_reply(request):
+
+
+@login_required
+def update_reply(request):
