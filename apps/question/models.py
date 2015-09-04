da8ecd0b0a5ae12b511d2c05a1571e400ca33462
Antonio Ladeia <antonioladeia@ideiaseo.com>
2015-09-04 14:08:16 -0300

    create models

--- apps/question/models.py
+++ apps/question/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+
+    title = models.CharField(max_length=256, blank=False)
+    description = models.CharField(min_length=200, max_length=2048, blank=False)
+    author = models.ForeignKey(User, blank=False)
+    question_date = models.DateField(auto_now=False, auto_now_add=True, blank=False)
+    correct_answer = models.OneToOneField(Answer)
+
+class Answer(models.Model):
+
+    description = models.CharField(min_length=200, max_length=2048, blank=False)
+    author = models.ForeignKey(User, blank=False)
+    answer_date = models.DateField(auto_now=False, auto_now_add=True, blank=False)
