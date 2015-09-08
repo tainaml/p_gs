dcb7f34f5b340ea46d7a7cf9059b12cdaa3d180c
phillip <philliparente@gmail.com>
2015-09-08 10:23:46 -0300

    Adding taxonomy into admin site

--- apps/taxonomy/admin.py
+++ apps/taxonomy/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import ObjectTaxonomy
+from .models import ObjectTaxonomy, Taxonomy, Term
 # Register your models here.
 
 admin.site.register(ObjectTaxonomy)
+admin.site.register(Taxonomy)
+admin.site.register(Term)
