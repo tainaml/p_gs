731e10cae5d153499c1743663c6e702aba17e152
phillip <philliparente@gmail.com>
2015-09-24 15:00:50 -0300

    Adding comment signal to notification

--- apps/rede_gsti_signals/signals/handlers.py
+++ apps/rede_gsti_signals/signals/handlers.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from apps.socialactions.models import UserAction
 from apps.article.models import Article
+from apps.comment.models import Comment
 
 from apps.notifications.service import business as Business
 
@@ -30,11 +32,23 @@ def social_action(sender, **kwargs):
                                            notification_action=action.action_type,
                                            target_object=action.content_object)
 
-@receiver(post_save, sender=Article)
-def my_callback(sender, **kwargs):
-    article = kwargs['instance']
+@receiver(post_save, sender=Comment)
+def comment_action(sender, **kwargs):
+    comment = kwargs['instance']
 
-    print "Yeah", article.title
+    if comment:
+        if comment.content_type.model in [
+            'article',
+            'comment'
+        ]:
+            to = comment.content_object.author
+            author = comment.author
+            if to != author:
+                Business.send_notification(author=author,
+                                           to=to,
+                                           notification_action=settings.SOCIAL_COMMENT,
+                                           target_object=comment.content_object)
 
-    print("Request finished!")
+
+
 
