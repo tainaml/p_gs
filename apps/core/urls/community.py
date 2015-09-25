ddef4296ccc5bcc016cf9eec64c9c31fc3936a6c
phillip <philliparente@gmail.com>
2015-09-25 10:13:53 -0300

    basic community feed

--- apps/core/urls/community.py
+++ apps/core/urls/community.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from ..views.community import views, CoreCommunityView, CoreCommunityFollowersView, CoreCommunityAboutView
+from ..views.community import views, CoreCommunityView, CoreCommunityFollowersView, CoreCommunityAboutView, CoreCommunityFeedView
 
 urlpatterns = [
-    url(r'^(?P<community_slug>[a-z0-9]+(?:(-|_)[a-z0-9]+)*)/$', CoreCommunityView.as_view(), name='show'),
+    url(r'^(?P<community_slug>[a-z0-9]+(?:(-|_)[a-z0-9]+)*)/$', CoreCommunityFeedView.as_view(), name='show'),
     url(r'^(?P<community_slug>[a-z0-9]+(?:(-|_)[a-z0-9]+)*)/followers/$', CoreCommunityFollowersView.as_view(), name='followers'),
     url(r'^(?P<community_slug>[a-z0-9]+(?:(-|_)[a-z0-9]+)*)/about/$', CoreCommunityAboutView.as_view(), name='about'),
+
     ]
--- apps/core/views/community.py
+++ apps/core/views/community.py
@@ -1,8 +1,10 @@
+from django.contrib.contenttypes.models import ContentType
 from apps.community import views
 from apps.community.models import Community
 from apps.socialactions.service.business import get_users_acted_by_model
+from apps.taxonomy.models import ObjectTaxonomy
 from rede_gsti import settings
-
+from apps.taxonomy.service import business as Business
 
 class CoreCommunityView(views.CommunityView):
 
@@ -33,3 +35,29 @@ class CoreCommunityFollowersView(CoreCommunityView):
 class CoreCommunityAboutView(CoreCommunityView):
 
     template_path = 'community/community-about.html'
+
+class CoreCommunityFeedView(CoreCommunityView):
+
+    def get_context(self, request, community_instance=None):
+        context = super(CoreCommunityFeedView, self).get_context(request, community_instance)
+        community_object_taxonomies = Business.get_taxonomies_by_model(community_instance)
+        taxonomies = [object_taxonomy.taxonomy for object_taxonomy in community_object_taxonomies]
+
+        content_types = ContentType.objects.filter(model__in=['article', 'question'])
+
+        object_taxonomies = ObjectTaxonomy.objects.filter(
+            taxonomy__in=taxonomies,
+            content_type__in=content_types,
+            content_object__status__in=[4]
+
+        ).distinct("object_id", "content_type").\
+            prefetch_related('content_object__author', 'content_object__author__profile')
+
+        for obj in object_taxonomies:
+            print obj.content_object
+        context.update({'object_taxonomies': object_taxonomies})
+
+        return context
+
+    template_path = 'community/community-view.html'
+
