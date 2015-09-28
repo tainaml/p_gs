ce6f4f04b000ea4b8aaa5b43796c60930e78fbd9
Raphael Fruneaux <raphaelfruneaux@Raphael.local>
2015-09-28 17:30:38 -0300

    template tag relevance_box

--- apps/core/templatetags/core_tags.py
+++ apps/core/templatetags/core_tags.py
@@ -2,30 +2,36 @@ from distutils.command.register import register
 
 from django import template
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import Count
 from django.http import Http404
+from django.utils import timezone
+
+from apps.article.models import Article
 from apps.feed.models import FeedObject
-
-from apps.taxonomy.service import business as TaxonomyBusiness
-
 
 
 register = template.Library()
 
 
 @register.inclusion_tag('core/templatetags/relevance-box.html', takes_context=True)
-def relevance_box(context, content_object, content_type, count=5, template_path='core/partials/relevance/article-base.html'):
+def relevance_box(context, content_object, count=4, template_path='core/partials/relevance/article-base.html'):
 
     try:
-        records_type = ContentType.objects.get(model=content_type)
+        record_type = ContentType.objects.get(model="article")
 
-        taxonomies = TaxonomyBusiness.get_taxonomies_by_model(content_object)
-        taxonomies_list = [tax.taxonomy.id for tax in taxonomies]
-
-        records = FeedObject.objects.filter(taxonomy__in=taxonomies_list, content_type=records_type)\
-                      .order_by('-articles__relevance',
-                                '-articles__publishin')\
-                      .distinct('articles__relevance', 'articles__publishin', 'object_id', 'content_type_id')[:count]
+        records = FeedObject.objects.filter(
+            taxonomies=content_object.taxonomy,
+            content_type=record_type,
+            article__status=Article.STATUS_PUBLISH,
+            article__publishin__lte=timezone.now()
+        ).order_by(
+            '-relevance',
+            '-article__publishin'
+        ).distinct(
+            'relevance',
+            'article__publishin',
+            'object_id',
+            'content_type_id'
+        )[:count]
 
     except ValueError:
         raise Http404()
