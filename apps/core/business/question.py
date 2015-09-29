4dd29ae0810eb653991892e8b5850f235c7fdfd5
Raphael Fruneaux <raphaelfruneaux@Raphael.local>
2015-09-29 16:27:15 -0300

    insert date in feed models + order feed by date

--- apps/core/business/question.py
+++ apps/core/business/question.py
@@ -8,5 +8,6 @@ def save_taxonomies(instance=None, data=None):
 
 def save_feed_item(instance, data=None):
     feed_object = BusinessFeed.feed_get_or_create(instance)
+    feed_object.date = instance.question_date
     feed_object.save()
     return feed_object
