be01e26ac7079e879cd65d68d73a4763111732ca
Raphael Fruneaux <raphaelfruneaux@Raphael.local>
2015-09-29 13:09:27 -0300

    fix merge and profile box

--- apps/userprofile/migrations/0003_auto_20150929_1300.py
+++ apps/userprofile/migrations/0003_auto_20150929_1300.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('userprofile', '0009_merge'),
+        ('userprofile', '0002_auto_20150928_1655'),
     ]
 
     operations = [
