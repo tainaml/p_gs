7fa797a87a14795dc1a3467df41907610567df08
Anniel Menezes <annielmenezes@ideiaseo.com>
2015-09-30 15:37:28 -0300

    Image upload component using drag'n drop feature

--- resources/src/javascripts/main.js
+++ resources/src/javascripts/main.js
@@ -1,7 +1,8 @@
+import './asyncModules'
+
 require('./vendor/bootstrap/')
-require('malihu-custom-scrollbar-plugin')($)
-const Dropzone = require('dropzone')
-const Selectize = require('selectize')
+// require('malihu-custom-scrollbar-plugin')($)
+// const Selectize = require('selectize')
 $(() => {
     require('./modules')
     $('#modal-personal-infos').modal('show')
@@ -12,21 +13,9 @@ $(() => {
         $(this).parents('[data-target=modal]').modal('hide')
     })
 
-    // custom file uploads
-    var gstiDropzone = new Dropzone('[data-toggle="dropzone"]', {
-        url: 'profile',
-        maxFiles: 1
-    })
-    gstiDropzone.on('addedfile', function (file) {
-        $('.dz-default.dz-message').hide()
-    })
-    .on("maxfilesexceeded", function(file) {
-        this.removeFile(file);
-    });
-
     // Custom scroll
-    $('[data-toggle="custom-scroll"]').mCustomScrollbar()
+    // $('[data-toggle="custom-scroll"]').mCustomScrollbar()
 
     // Custom tags
-    $('[data-toggle="selectize"]').Selectize();
+    // $('[data-toggle="selectize"]').Selectize();
 })
