e0a3cec892f1bcf0a8db8aa2cbe6d3d2f0300506
Antonio Ladeia <antonioladeia@ideiaseo.com>
2015-10-02 11:47:56 -0300

    keep data for merge

--- apps/core/business/user.py
+++ apps/core/business/user.py
@@ -2,6 +2,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from apps.feed.models import FeedObject
 from django.db.models import Q
+from apps.socialactions.service import business as socialBusiness
+from rede_gsti import settings
 
 
 def get_feed_objects(profile_instance=None, description=None, content_types_list=None, items_per_page=None, page=None):
@@ -11,9 +13,16 @@ def get_feed_objects(profile_instance=None, description=None, content_types_list
 
     content_types = ContentType.objects.filter(model__in=content_types_list)
 
+    communities = socialBusiness.get_users_acted_by_author(author=profile_instance.user,
+                                                     action=settings.SOCIAL_FOLLOW,
+                                                     content_type='community')
+    taxonomy_list = []
+    for community in communities:
+        taxonomy_list.append(community.content_object.taxonomy)
+
     feed_objects = FeedObject.objects.filter(
         Q(content_type__in=content_types) &
-        Q(taxonomies=profile_instance.taxonomy) &
+        Q(taxonomies__in=taxonomy_list) &
         (
             Q(article__title__icontains=description) |
             Q(article__text__icontains=description)  |
