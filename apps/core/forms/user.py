d601c8eb26673caed16a795f0caf1dd26401cca3
Antonio Ladeia <antonioladeia@ideiaseo.com>
2015-10-07 15:38:58 -0300

    finalize feed userprofile

--- apps/core/forms/user.py
+++ apps/core/forms/user.py
@@ -3,17 +3,19 @@ from ..business import user as Business
 
 
 class CoreUserSearchForm(IdeiaForm):
+
     criterio = forms.CharField(required=False)
     page = forms.IntegerField(required=False)
 
-    def __init__(self, profile_instance=None, content_types=None, itens_by_page=None, user=None, *args, **kwargs):
+    def __init__(self, profile_instance=None, content_types=None,
+        itens_by_page=None, user=None, *args, **kwargs):
+
         self.profile_instance=profile_instance
         self.content_types = content_types
         self.itens_by_page = itens_by_page
         self.user = user
 
         super(CoreUserSearchForm, self).__init__(*args, **kwargs)
-
 
     def clean(self):
         cleaned_data = super(CoreUserSearchForm, self).clean()
@@ -22,7 +24,6 @@ class CoreUserSearchForm(IdeiaForm):
             if 'page' in cleaned_data and cleaned_data['page'] else 1
 
         return cleaned_data
-
 
     def __process__(self):
 
@@ -33,4 +34,17 @@ class CoreUserSearchForm(IdeiaForm):
             self.itens_by_page,
             self.cleaned_data['page'],
             self.user
+        )
+
+
+class CoreUserProfileForm(CoreUserSearchForm):
+
+    def __process__(self):
+
+        return Business.get_articles_from_user(
+            profile_instance=self.profile_instance,
+            description=self.cleaned_data['criterio'],
+            content_type=self.content_types,
+            items_per_page=self.itens_by_page,
+            page=self.cleaned_data['page']
         )
--- apps/core/views/user.py
+++ apps/core/views/user.py
@@ -1,18 +1,27 @@
+from django.contrib.contenttypes.models import ContentType
 from django.http import Http404
 from django.shortcuts import render
-from apps.core.forms.user import CoreUserSearchForm
+from apps.core.forms.user import CoreUserSearchForm, CoreUserProfileForm
 from apps.userprofile.views import ProfileShowView
 
 
 class CoreUserSearchView(ProfileShowView):
 
     template_path = 'userprofile/profile.html'
+    form = CoreUserSearchForm
 
     def get_context(self, request, profile_instance=None):
-        context = super(CoreUserSearchView, self).get_context(request, profile_instance)
+        context = super(
+            CoreUserSearchView,
+            self
+        ).get_context(
+            request,
+            profile_instance
+        )
+
         itens_by_page = 10
 
-        self.form = CoreUserSearchForm(
+        form = self.form(
             profile_instance,
             ['article', 'question'],
             itens_by_page,
@@ -20,13 +29,17 @@ class CoreUserSearchView(ProfileShowView):
             request.GET
         )
 
-        feed_objects = self.form.process()
+        feed_objects = form.process()
 
-        context.update({'feed_objects': feed_objects, 'form': self.form, 'page': self.form.cleaned_data['page'] + 1})
+        context.update(
+            {'feed_objects': feed_objects,
+            'form': form,
+            'page': form.cleaned_data['page'] + 1
+        })
 
         return context
 
-    def get(self, request):
+    def get(self, request, username=None):
 
         profile = self.filter(request, request.user)
 
@@ -46,4 +59,39 @@ class CoreUserList(CoreUserSearchView):
 
 
 class CoreUserFeed(CoreUserSearchView):
+    template_path = 'userprofile/profile-feed.html'
+
+
+class CoreUserProfile(CoreUserSearchView):
     template_path = 'userprofile/profile.html'
+    form = CoreUserProfileForm
+
+    def get(self, request, username=None):
+
+        profile = self.filter(request, username)
+
+        context = {'profile': profile}
+        context.update(self.get_context(request, profile))
+
+        return render(request, self.template_path, context)
+
+    def get_context(self, request, profile_instance=None):
+        content_type = ContentType.objects.filter(model='article')
+
+        itens_by_page = 10
+
+        form = self.form(
+            profile_instance,
+            content_type[0].id,
+            itens_by_page,
+            profile_instance.user,
+            request.GET
+        )
+
+        feed_objects = form.process()
+
+        return {
+            'feed_objects': feed_objects,
+            'form': form,
+            'page': form.cleaned_data['page'] + 1
+        }
