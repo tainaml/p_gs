06c1e524dabfc48df584a9f6e99815a2c7c76c24
Anniel Menezes <annielmenezes@ideiaseo.com>
2015-11-05 16:00:22 -0300

    Correction on general menu

--- resources/src/javascripts/modules/mega-dropdown.js
+++ resources/src/javascripts/modules/mega-dropdown.js
@@ -2,27 +2,6 @@
 
 ;(function ($) {
     var navLinks = $('.menu-categories > li');
-    // navLinks.on({
-    //     'mouseenter': showMenu,
-    //     'mouseleave': function (event) {
-    //         var $menuArea = $(this).find('.menu-area');
-    //         event.preventDefault();
-    //         console.log('Mouse event type: %s', event.type);
-
-    //         $menuArea.slideUp('fast');
-    //         // return false;
-    //     }
-    // });
-
-    function showMenu (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        console.log('Mouse event type: %s', event.type);
-        var target = $(event.currentTarget).find('.menu-area');
-        // $('.menu-area').slideUp('fast');
-        $(target).slideDown();
-        // return false;
-    }
 
     var delay = 300, setTimeoutConst,
         delay2 = 100, setTimeoutConst2;
