f6d512680cb7a13cf2e5ba6a5bb14b2f71ad2298
phillip <philliparente@gmail.com>
2015-11-12 13:38:22 -0300

    removing cache on article

--- apps/core/urls/article.py
+++ apps/core/urls/article.py
@@ -12,5 +12,5 @@ urlpatterns = [
     url(r'^article/edit/(?P<article_id>\d+)$', _article_edit_view, name='edit'),
     url(r'^article/delete/(?P<article_id>\d+)$', _article_delete_view, name='delete'),
     url(r'^article/delete/async/(?P<article_id>\d+)$', views.ArticleDeleteAjax.as_view(), name='delete-async'),
-    url(r'^article/(?P<article_slug>[a-z0-9]+(?:-[a-z0-9]+)*)/(?P<article_id>\d+)$',cache_page(604800)(_article_show_view), name='view'),
+    url(r'^article/(?P<article_slug>[a-z0-9]+(?:-[a-z0-9]+)*)/(?P<article_id>\d+)$', _article_show_view, name='view'),
 ]
