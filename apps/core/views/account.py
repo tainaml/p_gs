9555aa6fe730dcb5fe250a334867782036c1f7b3
Jovem Elder <eldersantos@ideiaseo.com>
2015-11-16 18:26:15 -0300

    Modal de esqueci senha

--- apps/core/views/account.py
+++ apps/core/views/account.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.generic import View
+from apps.account.service.forms import ForgotPasswordForm
 
 from ..forms.account import CoreSignUpForm
 from apps.account import views
@@ -10,6 +12,27 @@ class CoreRegisterView(views.RegisterView):
 
 
 class CoreLoginView(views.LoginView):
+
+    def return_error(self, request, context=None):
+        _response_context = {}
+
+        if 'form' in context:
+            _form = context['form']
+            _active = _form.account_is_active if hasattr(_form, 'account_is_active') else None
+
+            _response_context = {
+                'errors': _form.errors,
+                'is_active': _active
+            }
+
+        return JsonResponse(_response_context, status=400)
+
+    def return_success(self, request, context=None):
+
+        return JsonResponse(context, status=200)
+
+
+class CoreForgotPassword(views.ForgotPasswordView):
 
     def return_error(self, request, context=None):
         _response_context = {}
