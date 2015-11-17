7c5624d788d913967e61f5ae32deac12191fd129
Raphael Fruneaux <raphaelfruneaux@ideiaseo.com>
2015-11-17 14:48:19 -0300

    completed task 3256

---
+++ apps/core/urls/contact.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from apps.core.views import contact as CoreContactViews
+
+urlpatterns = [
+
+    url(r'^$', CoreContactViews.CoreContactView.as_view(), name='create'),
+    url(r'^save/$', CoreContactViews.CoreContactSaveViews.as_view(), name='save'),
+
+]
