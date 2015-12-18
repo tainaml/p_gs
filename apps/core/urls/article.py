bb250b1647815e7aa350910eec9f64913c3cff8d
Roque Junior <roquejunior@ideiaseo.com>
2015-12-18 16:48:35 -0300

    article: create by category

--- apps/core/urls/article.py
+++ apps/core/urls/article.py
@@ -1,16 +1,19 @@
 from django.conf.urls import url
 from django.views.decorators.cache import cache_page
-from ..views.article import views, CoreArticleEditView, CoreArticleView
+from ..views.article import views, CoreArticleEditView, CoreArticleView, CoreArticleInCommunityView
 from apps.article import urls
 
 _article_show_view = views.ArticleView.as_view()
 _article_edit_view = CoreArticleEditView.as_view()
 _article_delete_view = views.ArticleDeleteView.as_view()
+_article_create_in_community = CoreArticleInCommunityView.as_view()
 
 urlpatterns = [
-    url(r'^article/create$', urls.article_edit_view, name='create'),
+    url(r'^article/create$', _article_edit_view, name='create'),
     url(r'^article/edit/(?P<article_id>\d+)$', _article_edit_view, name='edit'),
     url(r'^article/delete/(?P<article_id>\d+)$', _article_delete_view, name='delete'),
     url(r'^article/delete/async/(?P<article_id>\d+)$', views.ArticleDeleteAjax.as_view(), name='delete-async'),
     url(r'^article/(?P<article_slug>[a-z0-9]+(?:-[a-z0-9]+)*)/(?P<article_id>\d+)$', CoreArticleView.as_view(), name='view'),
+
+    url(r'^article/create/(?P<community_slug>[a-z0-9]+(?:-[a-z0-9]+)*)$', _article_create_in_community, name='create-in-category'),
 ]
--- apps/core/views/article.py
+++ apps/core/views/article.py
@@ -1,13 +1,26 @@
 import json
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from apps.article import views
+from apps.community.models import Community
+from apps.taxonomy.models import Taxonomy
 from ..forms.article import CoreArticleForm
 from ..business import feed as BusinessFeed
+from apps.core.business import user as UserBusiness
 
 class CoreArticleEditView(views.ArticleEditView):
 
     form_article = CoreArticleForm
+
+    def prepare_context(self, request, context):
+        context = super(CoreArticleEditView, self).prepare_context(request, context)
+        communities = UserBusiness.get_user_communities_list(request.user)
+
+        context.update(communities=communities)
+
+        return context
 
 
 class CoreArticleView(views.ArticleView):
@@ -17,3 +30,33 @@ class CoreArticleView(views.ArticleView):
         return {
             'feed': feed_object
         }
+
+class CoreArticleInCommunityView(CoreArticleEditView):
+
+    article_community = False
+
+    @method_decorator(login_required)
+    def get(self, request, community_slug=None, *args, **kwargs):
+
+        try:
+            self.article_community = Community.objects.filter(slug=community_slug)
+        except Community.DoesNotExist, e:
+            pass
+
+        return super(CoreArticleEditView, self).get(request, article_id=None, *args, **kwargs)
+
+    def prepare_initial_data(self, initial):
+        initial.update(communities=self.article_community)
+        return initial
+
+    def prepare_context(self, request, context):
+
+        context = super(CoreArticleInCommunityView, self).prepare_context(request, context)
+
+        form_article = context.get('form_article', None)
+
+        if form_article and 'communities' in form_article.fields:
+            form_article.fields["communities"].initial = self.article_community
+            context.update(form_article=form_article)
+
+        return context
