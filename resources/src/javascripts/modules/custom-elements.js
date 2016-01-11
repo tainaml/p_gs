5d706f373697ba46a67671500fe84b67db5bdd59
Raphael Fruneaux <raphaelfruneaux@ideiaseo.com>
2016-01-11 17:53:02 -0300

    fix sticky box-share

--- resources/src/javascripts/modules/custom-elements.js
+++ resources/src/javascripts/modules/custom-elements.js
@@ -21,3 +21,63 @@ $(document).on('click', function () {
         $('.custom-select-list').slideUp();
     }
 });
+
+
+function affixBoxShare() {
+
+    var $box = $('.box-share');
+    var $post = $('.full-post');
+    var $window = $(window);
+
+    var boxHeight = $box.outerHeight();
+    var boxPadding = 20;
+    var boxInitialTop = $box.offset().top;
+    var boxOffsetTopFixed = $box.offset().top - boxPadding;
+
+    var postHeight = $post.outerHeight();
+    var postOffsetBottom = $post.offset().top + postHeight;
+
+    var scrollize = function() {
+
+        if (postHeight != $post.outerHeight()) {
+            postHeight = $post.outerHeight();
+            postOffsetBottom = $post.offset().top + postHeight;
+        }
+
+        if ($window.scrollTop() >= boxOffsetTopFixed) {
+            if (!$box.hasClass('fixed')) {
+                $box.addClass('fixed').css({
+                    'position': 'fixed',
+                    'top': boxPadding
+                });
+            } else {
+                if (($window.scrollTop() + $box.outerHeight() + boxPadding) < postOffsetBottom) {
+                    if ($box.hasClass('fixed-relative')) {
+                        $box.removeClass('fixed-relative').css({
+                            'position': 'fixed',
+                            'top': boxPadding
+                        });
+                    }
+                } else {
+                    if (!$box.hasClass('fixed-relative')) {
+                        $box.addClass('fixed-relative').css({
+                            'position': 'absolute',
+                            'top': postOffsetBottom - boxHeight
+                        });
+                    }
+                }
+            }
+        } else {
+            if ($box.hasClass('fixed')) {
+                $box.removeClass('fixed').css({
+                    'position': 'absolute',
+                    'top': boxInitialTop
+                });
+            }
+        }
+    };
+
+    $(window).scroll(scrollize);
+    $(window).load(scrollize)
+}
+affixBoxShare();
