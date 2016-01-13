3dde4b28c459abb1542426c28a5023285cf4dfb7
phillip <philliparente@gmail.com>
2016-01-13 18:58:14 -0300

    complaint admin

--- apps/complaint/admin.py
+++ apps/complaint/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.complaint.models import ComplaintType, Complaint
+
+admin.site.register(ComplaintType)
+admin.site.register(Complaint)
