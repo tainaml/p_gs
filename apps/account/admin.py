12f5d2aef88c0ef362945c1b0dd6097e10220570
phillip <philliparente@gmail.com>
2016-03-14 15:55:11 -0300

    fixing get avatar

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,6 +1,15 @@
 # Register your models here.
 from django.contrib import admin
-
+from django.conf import settings
+from django.core.urlresolvers import reverse
 from apps.account.models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email','first_name', 'last_name','last_login','is_staff', 'is_active')
+    list_filter = ('username', 'first_name', )
+
+    def view_on_site(self, obj):
+        return settings.SITE_URL + reverse('profile:show',
+                                               args=[obj.username])
+
+admin.site.register(User, UserAdmin)
--- apps/core/templatetags/avatar_tags.py
+++ apps/core/templatetags/avatar_tags.py
@@ -15,12 +15,12 @@ def get_avatar(context, user, **kwargs):
     request = context.get('request')
 
     if user.is_active or (request.user == user and request.user.is_authenticated()):
-        if user.profile and user.profile.profile_picture and user.profile.profile_picture.name:
+        if hasattr(user, 'profile ') and user.profile.profile_picture and user.profile.profile_picture.name:
             image_url = '%s/%s' % (settings.THUMBOR_MEDIA_URL, user.profile.profile_picture.name)
         else:
-            image_url = get_avatar_path(user.profile.gender if user.profile.gender else 'M')
+            image_url = get_avatar_path(user.profile.gender if hasattr(user, 'profile ') and user.profile.gender else 'M')
     elif not user.is_active:
-        image_url = get_avatar_path(user.profile.gender if user.profile.gender else 'M')
+        image_url = get_avatar_path(user.profile.gender if hasattr(user, 'profile ') and user.profile.gender else 'M')
     else:
         image_url = get_avatar_path('M')
 
