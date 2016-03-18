6433602b2b1e82b2c6373b9c2632448b0e1c2dc8
Raphael Fruneaux <raphaelfruneaux@ideiaseo.com>
2016-03-18 11:38:08 -0300

    removendo a relação mutua de comunidade relacionadas

--- apps/community/admin.py
+++ apps/community/admin.py
@@ -6,5 +6,6 @@ from .models import Community
 class CommunityAdmin(admin.ModelAdmin):
     filter_horizontal = ['related']
     ordering = ['title']
+    search_fields = ['title']
 
 admin.site.register(Community, CommunityAdmin)
