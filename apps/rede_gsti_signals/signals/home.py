627c042b568021b558cba81db56f00bae50942b3
Roque Junior <roquejunior@ideiaseo.com>
2016-03-22 17:40:00 -0300

    andamento

--- apps/rede_gsti_signals/signals/home.py
+++ apps/rede_gsti_signals/signals/home.py
@@ -24,12 +24,27 @@ def refresh_home_block(sender, **kwargs):
 
     feed_object = kwargs['instance']
 
+    if not feed_object.official or not feed_object.official__old_value:
+        return
+
     article_type = ContentType.objects.get_for_model(Article)
     if feed_object and feed_object.content_object and feed_object.content_type == article_type:
 
         logger.info("Iterating taonomies...")
         for tax in feed_object.taxonomies.all():
             logger.info("    >" + str(tax))
+
+
+            _home_caches = []
+
+            home_excludes = cache.get('global_home_excludes')
+            if home_excludes:
+                for home_exclude in home_excludes:
+                    _home_caches.append(home_exclude)
+                    _delete_excludes = 'home_block_excludes:%s' % home_exclude
+                    cache.delete(_delete_excludes)
+
+                cache.delete('global_home_excludes')
 
             # Limpando cache que evita duplicata na home.
             excludes_key = 'home_block_excludes_%s' % tax.slug.lower()
@@ -38,9 +53,13 @@ def refresh_home_block(sender, **kwargs):
 
             for key_name in block_keys:
                 logger.info("        >" + key_name)
-                # temp_key = make_template_fragment_key(key_name, (
-                #     tax.slug.lower()
-                # ))
-                # workaround
-                temp_key = key_name + "-" + tax.slug.lower()
-                cache.delete(temp_key)
+
+                for _home_cache in _home_caches:
+
+                    temp_key = '%s-%s-%s' % (
+                        key_name,
+                        _home_cache,
+                        tax.slug.lower()
+                    )
+
+                    cache.delete(temp_key)
