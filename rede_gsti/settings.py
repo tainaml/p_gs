d16e8a829194442b4ed987f3668ee58da79cd2e5
Raphael Fruneaux <raphaelfruneaux@ideiaseo.com>
2016-03-24 10:04:26 -0300

    Adicionar validação de senha

--- rede_gsti/settings.py
+++ rede_gsti/settings.py
@@ -227,6 +227,26 @@ TEMPLATES = [
     },
 ]
 
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
 WSGI_APPLICATION = '%s.wsgi.application' % PROJECT_NAME
 
 # Populate file
