ab4fafd9230d095e4b3526030f8f18d02c0cbaee
Raphael Fruneaux <raphaelfruneaux@ideiaseo.com>
2016-04-01 15:01:53 -0300

    fix import

--- apps/core/views/account.py
+++ apps/core/views/account.py
@@ -2,7 +2,6 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
 
-from ..forms.account import CoreSignUpForm
 from apps.account import views
 
 
