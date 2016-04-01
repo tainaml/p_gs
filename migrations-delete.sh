c803559bfdde48c19da0321ed8c1e15d2daae81b
Raphael Fruneaux <raphaelfruneaux@ideiaseo.com>
2016-04-01 14:29:27 -0300

    correção de migrations

--- migrations-delete.sh
+++ migrations-delete.sh
@@ -3,6 +3,6 @@
 echo ">> Starting ..."
 
 echo ">> Deleting old migrations"
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.p*" -not -name "__init__.py" -delete
 
 echo ">> Done"
