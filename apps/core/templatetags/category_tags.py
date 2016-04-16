37db5447192b7581b62120920fc0286c02cd01dc
Raphael Fruneaux <raphaelfruneaux@ideiaseo.com>
2016-04-16 14:39:39 -0300

    cache da templatetag

--- apps/core/templatetags/category_tags.py
+++ apps/core/templatetags/category_tags.py
@@ -1,12 +1,9 @@
 from django import template
-from django.core.cache import cache
 
 from apps.community.models import Community
 from apps.taxonomy.service.business import get_related_list_top_down
 
 register = template.Library()
-
-# cache = caches['default']
 
 
 @register.inclusion_tag('core/templatetags/communities-related.html', takes_context=True)
@@ -17,18 +14,8 @@ def communities_related(context, category, count=5):
 
     count = 5 if count > 10 else count
 
-    cache_prefix = 'cached_communities_related_'
-    cache_key = "{cache_prefix}{category_slug}".format(
-        cache_prefix=cache_prefix,
-        category_slug=category.slug
-    )
-
-    communities = cache.get(cache_key, [])
-
-    if not communities:
-        taxonomies = get_related_list_top_down([category])
-        communities = Community.objects.filter(taxonomy__in=taxonomies).exclude(taxonomy=category).order_by('?')[:count]
-        cache.set(cache_key, communities, 60 * 60)
+    taxonomies = get_related_list_top_down([category])
+    communities = Community.objects.filter(taxonomy__in=taxonomies).exclude(taxonomy=category).order_by('?')[:count]
 
     response_data = {
         'communities': communities,
