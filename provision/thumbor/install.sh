9672d080ecf341fef1a6ccb031f729c9aa0486a4
phillip <philliparente@gmail.com>
2016-04-27 17:09:25 -0300

    removing celery, adding new libs

--- provision/thumbor/install.sh
+++ provision/thumbor/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get update 
+apt-get update
+sudo apt-get install libssl-dev
 apt-get install -y build-essential python-dev curl python-pycurl python-pip
 apt-get install -y python-numpy python-opencv webp libpng-dev libtiff-dev libjasper-dev libjpeg-dev
 apt-get install -y libdc1394-22-dev libdc1394-22 libdc1394-utils
