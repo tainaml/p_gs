e86b8c7016a18d5fc945e927ef38b6dac2e9be89
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-04-27 14:05:38 -0300

    #4971 - Layout da página de listagem de vagas

--- apps/job_vacancy/urls.py
+++ apps/job_vacancy/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+
 urlpatterns = [
-    url(r'^', views.do_list, name='list'),
+    url(r'^job/', views.job, name='job'),
+    url(r'$', views.do_list, name='list'),
 ]
