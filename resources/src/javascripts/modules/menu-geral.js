7431aa2fc8fccd85e544767b65c03105a9732da3
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-05-11 18:11:07 -0300

    #5183 - Menu de vagas

--- resources/src/javascripts/modules/menu-geral.js
+++ resources/src/javascripts/modules/menu-geral.js
@@ -1,6 +1,6 @@
 ;(function () {
   'use strict';
-  var $header = $('.header-navegation');
+  var $header = $('.js-navigation');
   var $toggle_categories = $header.find('.toggle_menu-categories');
   var showHeaderMenu = function (event) {
     $header.toggleClass('is-oppened');
