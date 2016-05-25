2c112da7481e7f8e9b56f21b9a0c98fce0ad5710
phillip <philliparente@gmail.com>
2016-05-25 11:23:02 -0300

    Addinf index to article

--- apps/article/models.py
+++ apps/article/models.py
@@ -23,14 +23,14 @@ class Article(models.Model):
 
     title = models.CharField(blank=False, null=False,
                              max_length=settings.ARTICLE_TITLE_LIMIT if hasattr(settings, "ARTICLE_TITLE_LIMIT") else 100)
-    slug = models.SlugField(default='', null=False, max_length=150)
+    slug = models.SlugField(default='', null=False, max_length=150, db_index=True)
     text = RichTextField(null=False, max_length=settings.ARTICLE_TEXT_LIMIT if hasattr(settings, "ARTICLE_TEXT_LIMIT") else 10000)
     image = models.ImageField(max_length=100, upload_to='article/%Y/%m/%d', blank=True, default='')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, related_name='articles', verbose_name=_('Author'))
 
     createdin = models.DateTimeField(null=False, auto_now_add=True)
     updatein = models.DateTimeField(null=False, auto_now=True)
-    publishin = models.DateTimeField(null=True)
+    publishin = models.DateTimeField(null=True, db_index=True)
 
     status = models.IntegerField(choices=STATUS_CHOICES, null=False)
 
