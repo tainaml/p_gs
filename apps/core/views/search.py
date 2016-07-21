ecec2dac986655a0c09836506da5d5ca6f69c19c
Antonio Ladeia <contato@antonioladeia.com>
2016-07-21 10:33:40 -0300

    bugfix feature 6026

--- apps/core/views/search.py
+++ apps/core/views/search.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
-from apps.taxonomy.models import Taxonomy
+from apps.taxonomy.models import Taxonomy, Term
 from apps.userprofile.service import business as BusinessUserProfile
 
 from ..forms import search as Forms
@@ -119,7 +119,8 @@ class SearchList(SearchBase):
         context = {}
 
         if 'category' in request.GET:
-            category = Taxonomy.objects.get(slug=request.GET['category'], term=1)
+            term = Term.objects.get(slug="categoria")
+            category = Taxonomy.objects.get(slug=request.GET['category'], term=term)
             context.update({"category":category.slug})
 
         try:
