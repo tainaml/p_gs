ef59e7163088272b571679ceb455ffee57d1329e
phillip <philliparente@gmail.com>
2016-07-25 11:14:45 -0300

    fixing gome cache signal

--- apps/rede_gsti_signals/signals/home.py
+++ apps/rede_gsti_signals/signals/home.py
@@ -15,17 +15,6 @@ cache = caches['default']
 def refresh_home_block(sender, **kwargs):
     logger.info("Refreshing home block...")
 
-
-    # key_name = 'home-block-taxonomy'
-    # block_keys = [
-    #     'home_block_full_width',
-    #     'home_block_half_two',
-    #     'home_block_half_three',
-    #     'home_block_third',
-    #     'home_block_highlight',
-    #     'home_block_article_home',
-    # ]
-
     feed_object = kwargs['instance']
 
     if not feed_object.official or not feed_object.official__old_value:
@@ -43,30 +32,10 @@ def refresh_home_block(sender, **kwargs):
         for tax in feed_object.taxonomies.all():
             logger.info("    >" + str(tax))
 
-            _home_caches = []
 
-            home_excludes = cache.get('global_home_excludes')
-            if home_excludes:
-                for home_exclude in home_excludes:
-                    _home_caches.append(home_exclude)
-                    cache.delete(home_exclude)
+        home_excludes = cache.get('global_home_excludes')
+        if home_excludes:
+            for home_exclude in home_excludes:
+                cache.delete(home_exclude)
 
-            print _home_caches
-            cache.delete('global_home_excludes')
-
-            # # Limpando cache que evita duplicata na home.
-            # excludes_key = 'home_excludes__%s' % tax.slug.lower()
-            # cache.delete(excludes_key)
-
-            # for key_name in block_keys:
-            #     logger.info("        >" + key_name)
-            #
-            #     for _home_cache in _home_caches:
-            #
-            #         temp_key = '%s-%s-%s' % (
-            #             key_name,
-            #             _home_cache,
-            #             tax.slug.lower()
-            #         )
-            #
-            #         cache.delete(temp_key)
+        cache.delete('global_home_excludes')
