8b19d87ecafb473bbc84e7b4dc2255d530684514
phillip <philliparente@gmail.com>
2016-08-17 11:45:18 -0300

    adding assets to admin form

--- apps/community/admin.py
+++ apps/community/admin.py
@@ -13,6 +13,19 @@ class ComunnityAdminForm(forms.ModelForm):
         slug = unicode(slug).lower() if slug else None
         return slug
 
+    class Media:
+        js = (
+            'https://code.jquery.com/jquery-2.2.4.min.js',
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.min.js', )
+
+        css= {
+            'all': (
+                'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css',
+            )
+        }
+
     class Meta:
         excludes = ()
         widgets = {
