41470b608d3fd9c5f636dc0ba48691ab882cb010
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-08-22 11:12:37 -0300

    Plugin para iniciar editores de texto, adição da funcao de sendFiles

--- resources/src/javascripts/modules/ideiaEditor/index.js
+++ resources/src/javascripts/modules/ideiaEditor/index.js
@@ -1,7 +1,45 @@
-$.fn.refreshEditors = function () {
+$.fn.refreshEditors = function (){
+
+  function sendFile(files, editor) {
+    var data = new FormData();
+    var $editor = $(editor);
+    $.each(files, function(i, value){
+      data.append("file" + i, value);
+    });
+
+    var csrftoken = getCookie('csrftoken');
+
+    $.ajax({
+      data: data,
+      type: "POST",
+      url: "/ideia-summernote/upload",
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(data) {
+        $.each(data.urls, function(i, value){
+          $editor.summernote('insertImage', value);
+        });
+      },
+      error: function(data){
+        if(data.status ==400 || data.status ==403){
+          alert(data.responseJSON.message);
+        }
+      },
+      beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+          xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+      }
+    });
+
+    return false;
+  }
+
   var $editors = this.find('[data-toggle="editor"]');
 
-  return $.each($editors, function(i, value){
+  $.each($editors, function(i, value){
+
     var $editor = $(value);
     var editorConfig = $editor.data('config') || {};
 
