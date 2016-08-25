bba8600be192e8c61067350965265079753112a0
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-08-25 10:20:25 -0300

    #6271 - Target blank no editor não está funcionando

--- resources/src/javascripts/vendor.js
+++ resources/src/javascripts/vendor.js
@@ -3,5 +3,7 @@ require('summernote');
 require('summernote/lang/summernote-pt-BR');
 require('./vendor/summernote/plugins/summernote-oembed-plugin');
 require('./vendor/summernote/plugins/summernote-prettyprint-plugin');
+require('./modules/ideiaEditor');
 
 PR.prettyPrint();
+$('[data-target="container-editor"]').refreshEditors();
--- static/javascripts/vendor.js
+++ static/javascripts/vendor.js
@@ -1,4 +1,4 @@
-webpackJsonp([2],{0:function(e,t,n){e.exports=n(76)},76:function(e,t,n){"use strict";n(77),n(78),n(81),n(82),n(83),PR.prettyPrint()},77:function(e,t){/**
+webpackJsonp([2],{0:function(e,t,n){e.exports=n(76)},76:function(e,t,n){(function(e){"use strict";n(77),n(78),n(81),n(82),n(83),n(45),PR.prettyPrint(),e('[data-target="container-editor"]').refreshEditors()}).call(t,n(2))},77:function(e,t){/**
 	 * @license
 	 * Copyright (C) 2006 Google Inc.
 	 *
