639a21d0ecca3f8e4c1dc990c1233b94d8702969
phillip <philliparente@gmail.com>
2016-08-29 12:05:47 -0300

    change settings

--- rede_gsti/settings.py
+++ rede_gsti/settings.py
@@ -725,7 +725,7 @@ SUMMERNOTE_CONFIG = {
         'article': {
             'airMode': False,
             'lang': 'pt-BR',
-            'minHeight': 350,
+            'minHeight': 340,
             'oEmbed': {
                 'service': '/oembed/',
                 'spinner': '<div class=\"text-center\"><img src=\"/static/images/preload.gif\" /></div>',
