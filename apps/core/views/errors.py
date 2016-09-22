6f3f7946a14c061b1f49171473375021d46db4bc
phillip <philliparente@gmail.com>
2016-09-22 14:38:28 -0300

    fixing errors

--- apps/core/views/errors.py
+++ apps/core/views/errors.py
@@ -3,28 +3,24 @@ from django.template import RequestContext
 
 
 def handler400(request):
-    response = render_to_response('400.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('400.html', {})
     response.status_code = 400
     return response
 
 
 def handler403(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('404.html', {})
     response.status_code = 404
     return response
 
 
 def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('404.html', {})
     response.status_code = 404
     return response
 
 
 def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('500.html', {})
     response.status_code = 500
     return response
