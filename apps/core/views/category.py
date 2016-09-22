2bc17ac8b61378bb7507265b7429eb1f44eb7f62
Roque Junior <roquejunior@ideiaseo.com>
2016-09-22 11:59:44 -0300

    new category home

--- apps/core/views/category.py
+++ apps/core/views/category.py
@@ -1,5 +1,10 @@
+from apps.article.models import Article
+from apps.core.business.content_types import ContentTypeCached
+from django.core.cache import cache
+from django.db.models import Q, Prefetch
 from django.http import Http404
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import View
 from django.utils.translation import gettext
 from apps.community.models import Community
@@ -10,6 +15,8 @@ from apps.taxonomy.models import Taxonomy
 
 
 class CoreCategoryPageView(View):
+
+    LIMIT_ITEMS = 11
 
     base_template = 'home/categorias/%s.html'
     default_template = 'default'
@@ -36,18 +43,61 @@ class CoreCategoryPageView(View):
 
         return self.context
 
+    def get_communities(self):
+        communities_filters = Q(taxonomy__parent=self.category) | Q(taxonomy=self.category)
+        communities = Community.objects.filter(communities_filters)
+        return communities
+
+    def get_feed(self):
+
+        cache_key = 'gsti|excludes|{}'.format(self.category.slug)
+        cached_feed = cache.get(cache_key)
+        if cached_feed:
+            return cached_feed
+
+        communities = self.get_communities()
+        article_type = ContentTypeCached.objects.get(model="article")
+
+        articles = Article.objects.filter(
+            Q(
+                feed__official=True,
+                feed__communities__in=communities,
+                status__in=[Article.STATUS_PUBLISH],
+                feed__content_type=article_type,
+                publishin__lte=timezone.now(),
+            )
+        ).order_by(
+            '-publishin'
+        ).prefetch_related(
+            Prefetch('feed__communities', queryset=communities),
+        ).distinct()[0:self.LIMIT_ITEMS]
+
+        cache.set(cache_key, articles, 500)
+
+        return articles
+
+
     def get(self, request, category_slug):
 
         try:
 
-            self.category = Taxonomy.objects.get(slug=category_slug, term__slug='categoria')
-            self.category.community_related = Community.objects.get(slug=category_slug, taxonomy__term__slug='categoria')
+            self.category = Taxonomy.objects.prefetch_related(
+                'community_related'
+            ).select_related(
+                'community_related'
+            ).get(
+                slug=category_slug,
+                term__slug='categoria'
+            )
+
+            # self.category.community_related = Community.objects.get(slug=category_slug, taxonomy__term__slug='categoria')
         except Taxonomy.DoesNotExist:
             raise Http404(gettext('Category not found or not root category.'))
 
         self.get_context().update({
             'category': self.category,
-            'category_slug': category_slug
+            'category_slug': category_slug,
+            'articles': self.get_feed()
         })
 
         return render(request, self.get_template(), self.get_context())
