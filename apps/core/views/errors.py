ce6ae3969b5fd7927ad988dc18b00c7d4307f57e
phillip <philliparente@gmail.com>
2016-09-22 16:18:18 -0300

    handler errors

--- apps/core/views/errors.py
+++ apps/core/views/errors.py
@@ -1,26 +1,18 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 
 
-
-def handler400():
-    response = render_to_response("400.html")
-    response.status_code = 400
-    return response
+def handler400(request):
+    return render(request, "400.html")
 
 
-def handler403():
+def handler403(request):
     response = render_to_response('404.html')
-    response.status_code = 404
-    return response
+    return render(request, "404.html")
 
 
-def handler404():
-    response = render_to_response('404.html')
-    response.status_code = 404
-    return response
+def handler404(request):
+    return render(request, "404.html")
 
 
-def handler500():
-    response = render_to_response('500.html')
-    response.status_code = 500
-    return response
+def handler500(request):
+    return render(request, "500.html")
