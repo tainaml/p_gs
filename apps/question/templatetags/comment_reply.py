911e0a647bb226c639d7e25e4823fcf966eb6131
Roque Junior <roquejunior@ideiaseo.com>
2016-09-28 10:54:38 -0300

    Answers

--- apps/question/templatetags/comment_reply.py
+++ apps/question/templatetags/comment_reply.py
@@ -1,3 +1,4 @@
+from apps.question.service.forms import EditAnswerForm
 from django.http import Http404
 from django import template
 
@@ -18,4 +19,21 @@ def list_reply(context, question_id):
     return {
         'list_comment': list_comment,
         'request': context['request']
+    }
+
+
+@register.inclusion_tag('question/edit_answer.html', takes_context=True)
+def answer_edit_box(context, answer):
+
+    user = None
+
+    request = context.get('request')
+    if request:
+        user = request.user
+
+    form = EditAnswerForm(instance=answer, user=user)
+
+    return {
+        'form': form,
+        'answer': answer
     }
