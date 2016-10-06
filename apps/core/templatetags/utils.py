e74ac05c06a5199ea61cc43238e6e8edbb5037f9
phillip <philliparente@gmail.com>
2016-10-06 13:34:29 -0300

    fixing tagmanager

--- apps/core/templatetags/utils.py
+++ apps/core/templatetags/utils.py
@@ -2,8 +2,17 @@ from django import template
 from django.conf import settings
 register = template.Library()
 
-@register.inclusion_tag("core/templatetags/tag-manager.html")
-def tag_manager():
+@register.inclusion_tag("core/templatetags/tag-manager-script.html")
+def tag_manager_script():
+    must_be_rendered = (settings.ENVIRONMENT == 'production')
+
+
+    return {
+        'must_be_rendered': must_be_rendered
+    }
+
+@register.inclusion_tag("core/templatetags/tag-manager-iframe.html")
+def tag_manager_iframe():
     must_be_rendered = (settings.ENVIRONMENT == 'production')
 
 
