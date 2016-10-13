fbbb49e15ae185318511f75066a1f3284e5f7a5d
Roque Junior <roquejunior@ideiaseo.com>
2016-10-13 10:54:41 -0300

    #6695: Feito. Detalhes no ticket

--- apps/core/templatetags/amp_tags.py
+++ apps/core/templatetags/amp_tags.py
@@ -17,16 +17,25 @@ def amp_normalize_text(context, text):
 
     ALLOWED_TAGS = [
         'p', 'img', 'video', 'audio', 'iframe',
-        'button', 'a', 'svg', 'ul', 'li', 'ol'
+        'button', 'a', 'svg', 'ul', 'li', 'ol',
+        'strike', 'b', 'i', 'hr',
+        'pre', 'code', 'blockquote',
+        'table', 'tbody', 'tr', 'td',
+        'h2', 'h3', 'h4', 'h5'
     ]
 
     attrs = {
         '*': [],
         'img': ['src'],
-        'iframe': ['src']
+        'iframe': ['src'],
+        'a': ['href'],
+        'code': ['class'],
+        'span': ['class'],
     }
 
-    styles = []
+    styles = [
+        'text-align'
+    ]
 
     cleaned = bleach.clean(
         text,
@@ -36,12 +45,15 @@ def amp_normalize_text(context, text):
         strip=True
     )
 
-    cleaned = re.sub(
-        '(<img).+(src="(?P<image_src>/media/uploads(.*)))"',
-        r'<img \2 src=\"{}\3\"'.format(''),
-        cleaned
-    )
-    cleaned = re.sub('(\<img)', r'<amp-img layout="responsive" height="320px" width="320px" ', cleaned)
-    # cleaned = re.sub('\<iframe', '<amp-iframe layout="responsive" sandbox="allow-scripts" ', cleaned)
+    # cleaned = re.sub(
+    #     '(<img).+(src="(?P<image_src>/media/uploads(.*)))"',
+    #     r'<img \2 src=\"{}\3\"'.format(''),
+    #     cleaned
+    # )
+
+    cleaned = re.sub('<iframe', '<amp-iframe layout="responsive" sandbox="allow-scripts allow-same-origin" height="212" width="320" ', cleaned)
+    cleaned = re.sub('</iframe>', '</amp-iframe>', cleaned)
+
+    cleaned = re.sub('(\<img )', r'<amp-img layout="responsive" height="200" width="320" ', cleaned)
 
     return cleaned
