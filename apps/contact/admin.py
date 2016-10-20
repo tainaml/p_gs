ba8975caab7dab61cda0b746b7e3ddec4f0dbfbe
Roque Junior <roquejunior@ideiaseo.com>
2016-10-20 14:49:58 -0300

    #6784: Done

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
 from .models import Contact, ContactSubject
+from .service.forms import ContactAdminForm
 
-admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+
+    form = ContactAdminForm
+
+
+admin.site.register(Contact, ContactAdmin)
 admin.site.register(ContactSubject)
