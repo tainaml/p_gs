2422dd86f888098767783d13f976d2ff7e72600d
phillip <philliparente@gmail.com>
2016-10-26 10:29:44 -0300

    adding analyticis to amp

--- apps/core/templatetags/utils.py
+++ apps/core/templatetags/utils.py
@@ -11,6 +11,24 @@ def tag_manager_script():
         'must_be_rendered': must_be_rendered
     }
 
+@register.inclusion_tag("core/templatetags/amp-tag-manager-script.html")
+def amp_tag_manager_script():
+    must_be_rendered = (settings.ENVIRONMENT == 'production')
+
+
+    return {
+        'must_be_rendered': must_be_rendered
+    }
+
+@register.inclusion_tag("core/templatetags/amp-tag-manager-iframe.html")
+def amp_tag_manager_iframe():
+    must_be_rendered = (settings.ENVIRONMENT == 'production')
+
+
+    return {
+        'must_be_rendered': must_be_rendered
+    }
+
 @register.inclusion_tag("core/templatetags/tag-manager-iframe.html")
 def tag_manager_iframe():
     must_be_rendered = (settings.ENVIRONMENT == 'production')
