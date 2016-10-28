59adbbd5041fa9bfe951db1ffe2691f61a2c2e80
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-10-28 09:04:35 -0300

    #6844 - Troca do package gulp-minify-css pelo gulp-clean-css, o anterior foi depreciado

--- resources/gulpfile.js/tasks/sass.js
+++ resources/gulpfile.js/tasks/sass.js
@@ -1,16 +1,15 @@
 var gulp         = require('gulp');
 var browserSync  = require('browser-sync');
 var sass         = require('gulp-sass');
-var minifyCss    = require('gulp-minify-css');
 var handleErrors = require('../libraries/handleErrors');
 var config       = require('../config/sass');
 var autoprefixer = require('gulp-autoprefixer');
-// var minify = require('gulp-clean-css');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
   return gulp.src(config.src)
     .pipe(sass(config.settings))
-    .pipe(minifyCss())
+    .pipe(cleanCSS())
     .on('error', handleErrors)
     .pipe(autoprefixer(config.autoprefixer))
     .pipe(gulp.dest(config.dest))
