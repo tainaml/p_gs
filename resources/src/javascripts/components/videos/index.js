f1737aafcec7489ffc25b0a6c444c26322242a90
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-11-09 17:01:09 -0300

    #6924 - listagem de videos

---
+++ resources/src/javascripts/components/videos/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import $ from 'jquery';
+
+if ( $('[data-page="videos"]') ) {
+  // $( '[data-toggle="collapse"]' ).on( 'click tap', function ( event ) {
+  //   event.preventDefault();
+  //   let target = $( event.currentTarget ).prop( 'href' );
+  //   $( target ).toggle();
+  // });
+}
