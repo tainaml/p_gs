6ceed4733a61be9f15282c309c217a47be1346f4
phillip <philliparente@gmail.com>
2016-12-05 14:04:34 -0300

    course in admin

--- apps/core/admin/course.py
+++ apps/core/admin/course.py
@@ -1,5 +1,17 @@
+from django import forms
 from django.contrib import admin
-from apps.core.models.course import Course
+from django.contrib.admin import StackedInline
+from django.forms import inlineformset_factory
+from ideia_summernote.widget import SummernoteWidget
+from apps.core.models.course import Course, Curriculum
+
+
+
+class CurriculumInline(StackedInline):
+
+    model = Curriculum
+    extra = 0
+    min_num = 1
 
 
 class CoreCourseAdmin(admin.ModelAdmin):
@@ -7,5 +19,7 @@ class CoreCourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'rating', 'internal_author', 'updatein')
     list_display_links = list_display
 
+
+    inlines = [CurriculumInline]
 
 admin.site.register(Course, CoreCourseAdmin)
