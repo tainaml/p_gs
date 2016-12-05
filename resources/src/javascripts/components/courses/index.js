3b0a9a02e9499f2cdf2f5c050535372cf61b7dba
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-12-05 15:59:24 -0300

    #7005 - Submit do formulário quando muda o valor dos campos

--- resources/src/javascripts/components/courses/index.js
+++ resources/src/javascripts/components/courses/index.js
@@ -1,54 +1,10 @@
 if ( $( '[data-page="courses"]' ).length ) {
-  const $sideMenu = $( '[data-target="sidemenu"]' )
-  const $dispather = $( '[data-toggle="sidemenu"]' )
-  const $showMore = $( '[data-toggle="showmore"]' )
-  const ESC_KEYCODE = 27
-  const animateMenu = ( position = {
-    left: 0
-  }) => {
-    $sideMenu.animate( position )
-    $( 'body' ).toggleClass( 'modal-open' )
-  }
-
-
-  $dispather.on( 'click tap', ( event ) => {
-    animateMenu()
-  })
-
-  $sideMenu.on( 'click tap', ( event ) => {
-    if ( $( event.target ).is( '.side__menu' )) {
-      animateMenu({
-        left: '100%'
-      })
+  console.log( 'Troll' );
+  $( '[data-form-send]' ).on( 'change', function onChangeSelectSendVideoForm () {
+    var $self = $(this);
+    var $form = $self.closest( 'form[data-form-send-enables]' );
+    if ( $form.length ) {
+        $form.trigger('submit');
     }
-  })
-
-  $( window ).on( 'keydown', ( event ) => {
-    if ( event.keyCode == 27 ) {
-      animateMenu({
-        left: '100%'
-      })
-    }
-  })
-
-  $showMore.on( 'click tap', ( event ) => {
-    const $list = $( event.target ).siblings( '.list-unstyled' )
-    const $itemList = $list.find( '[data-hidden]' )
-
-    $itemList.toggleClass( function () {
-      $( this ).removeClass( 'item__visible item__hidden' )
-
-      if ( $( this ).data( 'hidden' ) ) {
-        $( this ).data( 'hidden', false )
-        $( event.target ).text( 'Fechar' )
-        return 'item__visible'
-      } else {
-        $( this ).data( 'hidden', true )
-        $( event.target ).text( 'mais 10...' )
-        return 'item__hidden'
-      }
-    })
-  })
-
-  $sideMenu.prependTo( 'body' )
+  });
 }
