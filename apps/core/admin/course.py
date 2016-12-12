bd0ddcb426f2890dc11d474347ad1f1ee8a87b34
phillip <philliparente@gmail.com>
2016-12-12 19:27:40 -0300

    removing footer and set course related rating

--- apps/core/admin/course.py
+++ apps/core/admin/course.py
@@ -8,7 +8,7 @@ from ideia_summernote.widget import SummernoteWidget
 from apps.core.models.course import Course, Curriculum
 from apps.core.models.plataform import Plataform
 from django.contrib.contenttypes.admin import GenericStackedInline
-
+from django.db import models
 
 class CurriculumInline(StackedInline):
 
@@ -16,12 +16,28 @@ class CurriculumInline(StackedInline):
     extra = 0
     min_num = 1
 
-
 class RatingsInline(GenericStackedInline):
 
     model = Rating
     extra = 1
     raw_id_fields = ['author']
+
+    # class Media:
+    #     js = (
+    #         'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+    #         'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.min.js', )
+    #
+    #     css= {
+    #         'all': (
+    #             'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+    #             'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css',
+    #         )
+    #     }
+    #
+    # class Meta:
+    #     widgets = {
+    #         'description': SummernoteWidget(editor_conf='article_admin'),
+    #     }
 
 
 class CoreCourseAdmin(admin.ModelAdmin):
@@ -34,7 +50,6 @@ class CoreCourseAdmin(admin.ModelAdmin):
     def view_on_site(self, obj):
 
         return reverse('course:show', args=[obj.slug])
-
 
 
 
