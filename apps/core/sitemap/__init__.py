1d7137facb5f5c5618c74a0be3ea23f1594805d2
Roque Junior <roquejunior@ideiaseo.com>
2016-12-12 17:12:44 -0300

    #7076: Done

--- apps/core/sitemap/__init__.py
+++ apps/core/sitemap/__init__.py
@@ -3,6 +3,7 @@ from .article import ArticleSitemap, ArticleOfficialSitemap
 from .question import QuestionSitemap
 from .community import CommunitySitemap
 from .category import CategorySitemap
+from .courses import CourseSitemap
 
 sitemaps = {
     'profiles': UserSitemap,
@@ -11,4 +12,5 @@ sitemaps = {
     'questions': QuestionSitemap,
     'communities': CommunitySitemap,
     'categories': CategorySitemap,
+    'courses': CourseSitemap
 }
