3d036e9f5bc925681b62ae9f845adbfc6ef13a8e
phillip <philliparente@gmail.com>
2016-12-13 09:20:06 -0300

    adding summernote widget into curriculum inline

--- apps/core/admin/course.py
+++ apps/core/admin/course.py
@@ -3,18 +3,42 @@ from apps.core.models.rating import Rating
 from django import forms
 from django.contrib import admin
 from django.contrib.admin import StackedInline
-from django.forms import inlineformset_factory
 from ideia_summernote.widget import SummernoteWidget
 from apps.core.models.course import Course, Curriculum
 from apps.core.models.plataform import Plataform
 from django.contrib.contenttypes.admin import GenericStackedInline
-from django.db import models
+
+class CurruculumAdminInlineForm(forms.ModelForm):
+
+
+    class Meta:
+        model = Curriculum
+        exclude = ()
+        widgets = {
+            'description': SummernoteWidget(editor_conf='article_admin')
+        }
+
+    class Media:
+        js = (
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.min.js', )
+
+        css= {
+            'all': (
+                'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css',
+            )
+        }
 
 class CurriculumInline(StackedInline):
+
+    form = CurruculumAdminInlineForm
 
     model = Curriculum
     extra = 0
     min_num = 1
+
+
 
 class RatingsInline(GenericStackedInline):
 
@@ -22,22 +46,7 @@ class RatingsInline(GenericStackedInline):
     extra = 1
     raw_id_fields = ['author']
 
-    # class Media:
-    #     js = (
-    #         'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
-    #         'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.min.js', )
-    #
-    #     css= {
-    #         'all': (
-    #             'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-    #             'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css',
-    #         )
-    #     }
-    #
-    # class Meta:
-    #     widgets = {
-    #         'description': SummernoteWidget(editor_conf='article_admin'),
-    #     }
+
 
 
 class CoreCourseAdmin(admin.ModelAdmin):
