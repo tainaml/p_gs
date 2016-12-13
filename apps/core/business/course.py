d707b62f173e62d75d7ace468bb1612b75fdea2c
phillip <philliparente@gmail.com>
2016-12-13 13:34:17 -0300

    fixing list filters

--- apps/core/business/course.py
+++ apps/core/business/course.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from apps.core.models.course import Course
 from apps.taxonomy.models import Taxonomy
 
@@ -19,19 +20,21 @@ def get_paginate_list(list=None, items_per_page=None, page=None):
 def get_courses(itens_per_page=None, **cleaned_data):
 
     filters = {}
+    criteria = Q(active=True)
     if cleaned_data.get('title'):
-        filters['title__contains']= cleaned_data.get('title')
+        criteria = criteria & Q(title__icontains=cleaned_data.get('title'))
 
     if cleaned_data.get('category'):
-        filters['taxonomies__parent']= Taxonomy.objects.filter(slug=cleaned_data['category'])
+        criteria = criteria & (Q(taxonomies__parent=Taxonomy.objects.filter(slug=cleaned_data['category']))
+                               | Q(taxonomies=Taxonomy.objects.filter(slug=cleaned_data['category'])))
 
     if cleaned_data.get('community'):
-        filters['taxonomies']= Taxonomy.objects.filter(slug=cleaned_data['community'])
+        criteria = criteria & Q(taxonomies=Taxonomy.objects.filter(slug=cleaned_data['community']))
 
-    order = cleaned_data.get('order') or "-updatein"
+    order = cleaned_data.get('order') or "-rating"
 
-
-    items = Course.objects.filter(**filters).prefetch_related("taxonomies", "languages", "internal_author__profile").order_by(order)
+    #TODO verify why is repeating rows
+    items = Course.objects.filter(criteria).prefetch_related("taxonomies", "languages", "internal_author__profile").distinct("id", order.replace("-", "")).order_by(order)
 
     return get_paginate_list(list=items, items_per_page=itens_per_page, page=cleaned_data['page'])
 
