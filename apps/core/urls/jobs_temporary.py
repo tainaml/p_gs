649d7ad244f0ef5e57f6662aa2fbb65f22b58c8a
phillip <philliparente@gmail.com>
2016-12-30 15:51:56 -0300

    url fix in job vacancy

--- apps/core/urls/jobs_temporary.py
+++ apps/core/urls/jobs_temporary.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
+from apps.job_vacancy.views import JobDetailView
 
 
 def __redirect_jobs_index(response):
@@ -10,4 +11,5 @@ def __redirect_jobs_index(response):
 
 urlpatterns = [
     url(r'^$', __redirect_jobs_index, name='index'),
+    url(r'^//(?P<slug>[a-z0-9./]+(?:(-|_)[a-z0-9.]+)*)/d+$', JobDetailView.as_view(), name='detail'),
 ]
