d9bc14e13a627769a30f3220f3508639dbc3384c
Anniel Menezes <annielmenezes@ideiaseo.com>
2017-01-04 15:21:32 -0300

    adição do jquery steps e do jquery validate nos passos da criação de organização

--- resources/src/javascripts/components/organization.js
+++ resources/src/javascripts/components/organization.js
@@ -1,87 +1,101 @@
+require('selectize')
 import input from './customform/input'
-import formset from '../vendor/jquery.formset'
-
+import ajaxselect from '../asyncModules/ajaxselect'
+import taxonomy from '../asyncModules/taxonomy'
+import dropzoneUploader from '../asyncModules/dropzoneUploader'
+import '../vendor/jquery.steps'
+import '../vendor/jquery.validate'
 module.exports = ( name ) => {
   const $root = $( name )
-
-  input( $root.find( '[data-toggle="input"]' ))
-  const $tab = $root.find( '[data-toggle="tab"]' )
-  const $tabContent = $( '[role=tabpanel]' )
-  const $user = $( '[name="user"]' )
-  const $role = $( '[name="permission"]' )
-  const $formset = $root.find( '[data-toggle="formset"]' )
+  const $steps = $root.find( '[data-toggle="steps"]' )
+  const $form = $root.find( '[data-toggle="form"]' )
   const $templateOrganizationUser = $('#template-organization-user')
-  const $addMember = $root.find( '[data-toggle="add-member"]' )
-  const totalForms = $('#id_' + $formset.data( 'name' ) + '-TOTAL_FORMS')
-  const maxForms = $('#id_' + $formset.data( 'name' ) + '-MAX_NUM_FORMS')
-  const minForms = $('#id_' + $formset.data( 'name' ) + '-MIN_NUM_FORMS')
   const childElementSelector = 'input,select,textarea,label,div'
   let usersId = []
-
-  $tabContent.not( '.active' ).hide()
-
-  $tab.on( 'click tap', '[role=tab]', ( event ) => {
-    event.preventDefault()
-
-    const $this = $( event.currentTarget )
-    const target =  $this.attr('href')
-
-    $this.addClass( 'active' )
-    $tabContent.fadeOut(() => {
-      $( target ).fadeIn()
-    })
-  })
-
-  $formset.find('[data-id]').each(( index, element ) => {
-    usersId.push( $(element).data( 'id' ))
-  })
-
-  $addMember.on( 'click', ( event ) => {
-    const $this = $( event.currentTarget )
-    const formPrefix = $formset.data( 'name' )
-    const formCount = parseInt(totalForms.val())
-    let userTemplate = $templateOrganizationUser.clone(true)
-
-
-    if ($user.val() && $role.val()) {
-      userTemplate.find( '[name $='+$user.attr('name')+']' ).val( $user.val() )
-      userTemplate.find( '[name $='+$role.attr('name')+']' ).val( $role.val() )
-      userTemplate.removeAttr('id')
-      userTemplate.removeProp('id')
-
-      if ( !(usersId.indexOf($user.val()) > -1)) {
-        userTemplate.find( childElementSelector ).each( (index, element) => {
-          updateElementIndex($(element), formPrefix, formCount)
-        })
-        totalForms.val( formCount + 1)
-        $( userTemplate ).find( '[data-target="name"]').text( $user.text())
-        $( userTemplate ).find( '[data-target="role"]').text( $role.find('option:selected').text())
-        $( userTemplate ).find( 'img').attr( 'src', `${location.origin}/perfil/dinamic-profile-image/${$user.val()}/70/`)
-        userTemplate.removeClass( 'hidden' )
-        userTemplate.data( 'id', $user.val())
-        $formset.append( userTemplate )
-        usersId.push($user.val())
-      }
-
-    } else {
-      alert( 'empty' )
+  $form.validate({
+    errorPlacement: function errorPlacement(error, element) {
+      element.after(error)
     }
   })
-
-  $formset.on( 'click', '.gsticon.gsticon-close', ( event ) => {
-    const $this = $( event.currentTarget )
-    const _user = $this.parent().parent();
-      usersId.some( user => {
-        console.log(user);
-        if ( user == _user.data( 'id' )) {
-          usersId.splice(usersId.indexOf(user), 1)
-          _user.hide()
+  $steps.children( '[role="tablist"]' ).steps({
+    headerTag: '.organization__register__tab',
+    bodyTag: '[role="tabpanel"]',
+    transitionEffect: 'slideLeft',
+    titleTemplate: '#title#',
+    labels: {
+        cancel: "Cancelar",
+        current: "passo atual:",
+        pagination: "Paginação",
+        finish: "Criar",
+        next: "Avançar",
+        previous: "Voltar",
+        loading: "Carregando ..."
+    },
+    onInit: function ( event, currentIndex ) {
+      const $user = $( '[name="user"]' )
+      const $role = $( '[name="permission"]' )
+      const $formset = $root.find( '[data-toggle="formset"]' )
+      const totalForms = $('#id_' + $formset.data( 'name' ) + '-TOTAL_FORMS')
+      const maxForms = $('#id_' + $formset.data( 'name' ) + '-MAX_NUM_FORMS')
+      const minForms = $('#id_' + $formset.data( 'name' ) + '-MIN_NUM_FORMS')
+      input( $root.find( '[data-toggle="input"]' ))
+      ajaxselect( $('[data-select="ajaxselect"]') )
+      taxonomy( $('[data-select="taxonomy"]') )
+      dropzoneUploader( $('[data-toggle="dropzoneUploader"]') )
+      $root.find( '[data-toggle="add-member"]' ).on( 'click', ( event ) => {
+        const $this = $( event.currentTarget )
+        const formPrefix = $formset.data( 'name' )
+        const formCount = parseInt(totalForms.val())
+        let userTemplate = $templateOrganizationUser.clone(true)
+        if ($user.val() && $role.val()) {
+          userTemplate.find( '[name $='+$user.attr('name')+']' ).val( $user.val() )
+          userTemplate.find( '[name $='+$role.attr('name')+']' ).val( $role.val() )
+          userTemplate.removeAttr('id')
+          userTemplate.removeProp('id')
+          if ( !(usersId.indexOf($user.val()) > -1)) {
+            userTemplate.find( childElementSelector ).each( (index, element) => {
+              updateElementIndex($(element), formPrefix, formCount)
+            })
+            totalForms.val( formCount + 1)
+            $( userTemplate ).find( '[data-target="name"]').text( $user.text())
+            $( userTemplate ).find( '[data-target="role"]').text( $role.find('option:selected').text())
+            $( userTemplate ).find( 'img').attr( 'src', `${location.origin}/perfil/dinamic-profile-image/${$user.val()}/70/`)
+            userTemplate.removeClass( 'hidden' )
+            userTemplate.data( 'id', $user.val())
+            $formset.append( userTemplate )
+            usersId.push($user.val())
+          }
+        } else {
+          alert( 'empty' )
         }
       })
-
+      $formset.find('[data-id]').each(( index, element ) => {
+        usersId.push( $(element).data( 'id' ))
+      })
+      $formset.on( 'click', '.gsticon.gsticon-close', ( event ) => {
+        const $this = $( event.currentTarget )
+        const _user = $this.parent().parent();
+        usersId.some( user => {
+          if ( user == _user.data( 'id' )) {
+            usersId.splice(usersId.indexOf(user), 1)
+            _user.hide()
+          }
+        })
+      })
+    },
+    onStepChanging: function ( event, currentIndex, newIndex ) {
+      $form.validate().settings.ignore = ':disabled,:hidden'
+      return $form.valid()
+    },
+    onFinishing: function ( event, currentIndex ) {
+      $form.validate().settings.ignore = ':disabled,:hidden'
+      return $form.valid()
+    },
+    onFinished: function ( event, currentIndex ) {
+      $form.submit()
+    }
   })
 };
-
 function updateElementIndex(elem, prefix, ndx) {
   const idRegex = new RegExp(prefix + '-(\\d+|__prefix__)-'),
       replacement = prefix + '-' + ndx + '-';
