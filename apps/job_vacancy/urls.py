fd833d4fbdda8955930bec000586ee6b1b57fef6
phillip <philliparente@gmail.com>
2017-01-17 15:01:25 -0300

    job vacation migrations

--- apps/job_vacancy/urls.py
+++ apps/job_vacancy/urls.py
@@ -6,9 +6,9 @@ from django.views.generic import RedirectView
 urlpatterns = [
     url(r'^(?P<job_slug>[a-z0-9]+(?:(-|_)[a-z0-9]+)*)/(?P<job_id>[0-9]+)$', views.JobDetailView.as_view(), name='detail'),
     url(r'^(?P<job_id>[0-9]+)/edit$', views.JobEditView.as_view(), name='edit'),
-    url(r'^create$', views.JobEditView.as_view(), name='create'),
-    url(r'^search$', views.JobView.as_view(), name='search'),
-    url(r'^list$', views.JobListView.as_view(), name='list'),
+    url(r'^criar', views.JobEditView.as_view(), name='create'),
+    url(r'^buscar$', views.JobView.as_view(), name='search'),
+    url(r'^listar$', views.JobListView.as_view(), name='list'),
     # url(r'^$', views.JobView.as_view(), name='index'),
     url(r'^$', RedirectView.as_view(pattern_name='community:show'), {'community_slug': "vagas-de-trabalho"}, name='index')
 ]
