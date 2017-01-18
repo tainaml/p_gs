80dce71373f72c7ea26e08a270ef07337ec74824
Anniel Menezes <annielmenezes@ideiaseo.com>
2017-01-18 15:39:56 -0300

    #7251 - Desabilitar os inputs quando não estão visíveis

--- resources/src/javascripts/components/customform/toggle-select.js
+++ resources/src/javascripts/components/customform/toggle-select.js
@@ -1,12 +1,14 @@
 module.exports = ( select ) => {
   const $select = $( select )
   const $target = $( '[data-salary]' )
+  $target.hide()
 
   $select.on({
     change: function ( event ) {
       const $this = $( event.currentTarget )
       $target.hide()
-      $( `[data-salary=${$this.val()}]` ).show()
+      $target.find(':input').attr('disabled', true)
+      $( `[data-salary=${$this.val()}]` ).show().find(':input' ).attr('disabled', false)
     }
   })
 }
