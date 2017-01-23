07de2c9f6bb517c5b6d903159e2ce8b22685da86
Anniel Menezes <annielmenezes@ideiaseo.com>
2017-01-23 10:01:30 -0300

    #7251 - Fix no comportamento do selectize de geography

--- resources/src/javascripts/components/geography.js
+++ resources/src/javascripts/components/geography.js
@@ -4,11 +4,7 @@ require('selectize');
 
 import $ from 'jquery';
 
-let geography,
-  elementData,
-  $element;
-
-var defaultSettings = {
+let defaultSettings = {
   plugins: ['remove_button'],
   maxItems: null,
   valueField: 'id',
@@ -53,53 +49,11 @@ var defaultSettings = {
   }
 };
 
-geography = ( element ) => {
+const geography = ( element ) => {
   const $element = $( element )
-  elementData = $element.data()
-  var selectizeSettings = $.extend( {}, defaultSettings, elementData )
+  const elementData = $element.data()
+  const selectizeSettings = $.extend( {}, defaultSettings, elementData )
   $element.selectize( defaultSettings )
-//   $element.selectize({
-//     valueField: 'url',
-//     labelField: 'name',
-//     searchField: 'name',
-//     create: false,
-//     render: {
-//       option: function(item, escape) {
-//           return '<div>' +
-//               '<span class="title">' +
-//                   '<span class="name"><i class="icon ' + (item.fork ? 'fork' : 'source') + '"></i>' + escape(item.name) + '</span>' +
-//                   '<span class="by">' + escape(item.username) + '</span>' +
-//               '</span>' +
-//               '<span class="description">' + escape(item.description) + '</span>' +
-//               '<ul class="meta">' +
-//                   (item.language ? '<li class="language">' + escape(item.language) + '</li>' : '') +
-//                   '<li class="watchers"><span>' + escape(item.watchers) + '</span> watchers</li>' +
-//                   '<li class="forks"><span>' + escape(item.forks) + '</span> forks</li>' +
-//               '</ul>' +
-//           '</div>';
-//       }
-//     },
-//     score: function(search) {
-//         var score = this.getScoreFunction(search);
-//         return function(item) {
-//             return score(item) * (1 + Math.min(item.watchers / 100, 1));
-//         };
-//     },
-//     load: function(query, callback) {
-//         if (!query.length) return callback();
-//         $.ajax({
-//             url: 'https://api.github.com/legacy/repos/search/' + encodeURIComponent(query),
-//             type: 'GET',
-//             error: function() {
-//                 callback();
-//             },
-//             success: function(res) {
-//               console.log(res.repositories);
-//                 callback(res.repositories.slice(0, 10));
-//             }
-//         });
-//     }
-// });
 };
 
 export default geography;
