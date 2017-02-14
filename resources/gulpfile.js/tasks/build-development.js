5f5005944d8e9d8d11d17012cbee4cba86076b36
Anniel Menezes <annielmenezes@ideiaseo.com>
2017-02-14 13:35:31 -0300

    #7385 - Remove unused javascript files

--- resources/gulpfile.js/tasks/build-development.js
+++ resources/gulpfile.js/tasks/build-development.js
@@ -2,5 +2,5 @@ var gulp         = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
 gulp.task('build:development', function(cb) {
-  gulpSequence(['clean', 'sass'], ['rev', 'webpack:development'], cb);
+  gulpSequence(['clean', 'sass'], ['webpack:development', 'rev'], cb);
 });
