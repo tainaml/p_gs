caaae6e6bd981d2fa61ab0343d2d27cff3cfb149
phillip <philliparente@gmail.com>
2018-02-28 08:38:08 -0300

    celery start

--- run-celery.sh
+++ run-celery.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DJANGO_DIR=/var/www/rede_gsti/
+DJANGO_DIR=/var/www/rede_gsti
 PROJECT=rede_gsti
 LOG_LEVEL=info
 
 cd $DJANGO_DIR
 export PROJECT_ENVIRONMENT=production
-source $DJANGO_DIRvenv/bin/activate
+source $DJANGO_DIR/venv/bin/activate
 celery -A $PROJECT worker -l $LOG_LEVEL
